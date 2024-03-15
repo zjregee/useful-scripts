1b2ec3ba635f56689aa20000a1895df53ba08eda
zjregee <zjregee@gmail.com>
2024-03-15 16:12:08 +0800

    update

---
+++ ubuntu-20.04-init.sh
@@ -0,0 +1,31 @@
+sudo apt-get update
+sudo apt-get upgrade
+sudo apt-get install build-essential
+sudo apt-get install cmake
+sudo apt-get install git
+sudo apt-get install net-tools
+sudo apt-get install curl
+sudo apt-get install openssh-server
+sudo apt-get install vim
+
+#### Docker
+### refer:
+##
+##
+
+#### Rust
+### refer:
+
+#### Golang
+### refer:
+
+#### Starfish
+### refer:
+
+##### Apps
+#####
+#### VScode
+###
+
+sudo snap install obsidian --classic
+sudo snap install typora
